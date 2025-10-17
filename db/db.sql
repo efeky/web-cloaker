a126ae50df4325108649ff59dc849c3ec310f9ea
efeky <162899730+efeky@users.noreply.github.com>
2025-10-17 22:26:39 +0300

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Oct 17, 2025 at 10:24 PM
--- Server version: 10.5.29-MariaDB-cll-lve
--- PHP Version: 8.3.8
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
